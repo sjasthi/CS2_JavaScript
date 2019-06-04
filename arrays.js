55c154af138ab042762410a5e1b345ef72f56d9d
Siva Jasthi <siva.jasthi@metrostate.edu>
2019-06-03 22:43:01 -0500

    array86 implemented
    function array86 (aaki satish) is now implemented.

--- arrays.js
+++ arrays.js
@@ -110,6 +110,40 @@ Author: Satish Aaki
    document.write("<br>-------------------");
    
 /* ----------------------------------------------------------------------
+8.6 Write a function to return the number of “odd” numbers in an array. (DONE)
+ ---------------------------------------------------------------------- */
+ function array86(number_array) {
+    var odd_number_count = 0;
+
+    for (var i = 0; i < number_array.length; i++) {
+    
+        // pseudo-code
+        // if the number from array is even, 
+        // then increase the count by 1
+        // if is is NOT even, 
+        // then ignore the number
+    
+        var temp_number = number_array[i];
+         
+         if (temp_number % 2 ==1) {
+            odd_number_count = odd_number_count+1;
+        }
+    }
+
+    return odd_number_count; 
+ }
+   
+   var array_86 = [2,3,44, 97, 46, 48, 87, 89, 65, 46, 46, 2, 4];
+   document.write("<br> Assignment 8.6");
+   document.write("<br> array_86 = " + array_86);
+   document.write("<br> Number of odd numbers in array_86 =  " + array86(array_86));
+   document.write("<br>-------------------");
+   
+
+
+
+
+/* ----------------------------------------------------------------------
 8.21 Write a function that creates N elements in array. 
 And the values of the array should be from 1 to N.
  ---------------------------------------------------------------------- */
