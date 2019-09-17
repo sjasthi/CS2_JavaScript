0e9da14ebbb764c9b5ef31f25b1ea8cb3195e328
Samy Rezzag <43894767+srezzag@users.noreply.github.com>
2019-09-16 20:12:37 -0500

    added 8.7

--- array.php
+++ array.php
@@ -137,6 +137,20 @@ function array84($array) {
    echo ("<br>-------------------");
    
 /*
+8.7 Reverse an inout array.
+*/
+function array87($number_array) {
+	$reverse_array = array_reverse($number_array,true);
+	print_r($reverse_array);
+}
+
+$input_array=[1,2,3,4,5,6,7,8,9,0 ];
+
+echo "<br>"."Reverse Array: ".print_r($input_array);
+
+$reverse_array = array87($input_array);
+
+/*
 8.8 Product				    
  */
 					    
