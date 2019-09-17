f3778f60467384e231fd069efd15f6b10b290782
Samy Rezzag <43894767+srezzag@users.noreply.github.com>
2019-09-16 20:07:48 -0500

    Update array.php

--- array.php
+++ array.php
@@ -139,7 +139,20 @@ Author: Satish Aaki
    document.write("<br> Number of odd numbers in array_86 =  " + array86(array_86));
    document.write("<br>-------------------");
    
+/* ----------------------------------------------------------------------
+8.7 Write a function to return the reverse of an array.
+ ---------------------------------------------------------------------- */
 
+function array87($number_array) {
+	$reverse_array = array_reverse($number_array,true);
+	print_r($reverse_array);
+}
+
+$input_array=[1,2,3,4,5,6,7,8,9,0 ];
+
+echo "<br>"."Reverse Array: ".print_r($input_array);
+
+$reverse_array = array87($input_array);
    
 /* ----------------------------------------------------------------------
 8.11 Write a function that returns union of two arrays. The combined array should not contain any duplicates.
